8bfe8cea01a60a7126076cb8fc07d18d1db34e8f
CheeseCake0427 <55739981+CheeseCake0427@users.noreply.github.com>
2022-06-08 15:42:36 +0800

    Update 1480. Running Sum of 1d Array.py
    Add topic description

--- 1480. Running Sum of 1d Array.py
+++ 1480. Running Sum of 1d Array.py
@@ -1,3 +1,11 @@
+# https://leetcode.com/problems/running-sum-of-1d-array/
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+# Return the running sum of nums.
+
+# Constraints:
+# 1 <= nums.length <= 1000
+# -10^6 <= nums[i] <= 10^6
+
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         numOfList = len(nums)
